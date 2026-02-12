78c7580feeacbb17afd9b2ccfc01e0d824617128
TeleGhost Dev <dev@teleghost.local>
2026-02-12 16:07:07 +0500

    Fix Android i2pd build: correctly set BOOST_LIBRARYDIR

--- scripts/build_i2pd_android.sh
+++ scripts/build_i2pd_android.sh
@@ -80,8 +80,9 @@ for i in "${!ARCHS[@]}"; do
         -DWITH_BINARY=OFF \
         -DWITH_STATIC=ON \
         -DOPENSSL_ROOT_DIR="$OPENSSL_ROOT/$ARCH" \
-        -DBoost_INCLUDE_DIR="$BOOST_INCLUDE" \
-        -DBoost_LIBRARY_DIRS="$BOOST_LIB" \
+        -DBOOST_ROOT="$BOOST_ROOT" \
+        -DBOOST_INCLUDEDIR="$BOOST_INCLUDE" \
+        -DBOOST_LIBRARYDIR="$BOOST_LIB" \
         -DBoost_NO_BOOST_CMAKE=ON \
         -DBoost_NO_SYSTEM_PATHS=ON \
         -DBoost_USE_STATIC_LIBS=ON \
